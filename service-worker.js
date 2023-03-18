4faf7168585b77879b09754fb034ecfdc77ff664
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-03-17 21:35:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ self.addEventListener('install', function(event) {
           '/index.html',
           '/estilo.css',
           '/logica.js',
-          '/404.html',
           'img/cores.png',
           
         ]);
