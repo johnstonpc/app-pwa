22daefd3fada37d6e99eae2da27e6a899fab3c3a
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-03-19 19:04:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,11 +12,32 @@ self.addEventListener('install', function(event) {
     );
   });
   
-  self.addEventListener('fetch', function(event) {
+
+  
+self.addEventListener('fetch', function(event) {
+  if (event.request.url === self.origin + '/' || event.request.url === self.origin + '/index.html') {
+    event.respondWith(
+      caches.open('meu-pwa').then(function(cache) {
+        return cache.match('index.html').then(function(cachedResponse) {
+          // Se a página estiver em cache, retorna ela
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+
+          // Caso contrário, pré-carrega a página e retorna ela
+          return fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  } else {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
       })
     );
-  });
-  
+  }
+});
+
