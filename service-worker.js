08801669ceeb6fd3a5c1742f714a21d0aa24672c
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-03-19 21:03:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,29 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('meu-pwa').then(function(cache) {
-        return cache.addAll([
-          '/index.html',
-          '/estilo.css',
-          '/logica.js',
-          'img/cores.png',
-          
-        ]);
-      })
-    );
-  });
-  
+const CACHE_NAME = "meu-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+   "https://johnstonpc.github.io/app-pwa/"
+   
+];
 
-  
-self.addEventListener('fetch', function(event) {
-  if (event.request.url === self.origin + '/' || event.request.url === self.origin + '/index.html') {
-    event.respondWith(
-      caches.open('meu-pwa').then(function(cache) {
-        return cache.match('index.html').then(function(cachedResponse) {
-          // Se a página estiver em cache, retorna ela
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-
-          // Caso contrário, pré-carrega a página e retorna ela
-          return fetch(event.request).then(function(response) {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  }
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      })
+  );
+});
