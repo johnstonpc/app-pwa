6653ae50eaa38476d1a483de6ad547e7d4b45a40
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-03-19 21:22:50 -0300

    Update logica.js

--- logica.js
+++ logica.js
@@ -53,4 +53,28 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/service-worker.js');
     });
   }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  showInstallButton();
+});
+
+function showInstallButton() {
+  const installButton = document.querySelector('#install-button');
+  installButton.style.display = 'block';
+  installButton.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Usuário aceitou a instalação');
+      } else {
+        console.log('Usuário rejeitou a instalação');
+      }
+      deferredPrompt = null;
+    });
+  });
+}
   
